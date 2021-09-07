144dc853ab3f3c5e2613e503878c37c41d8102ae
Nick <nick.alex.ritchie@gmail.com>
2021-09-07 18:44:06 +1000

    starting to experiment

--- controllers/graph-routes.js
+++ controllers/graph-routes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Transaction } = require('../models');
 const {onlyIfLoggedIn} = require('../middleware/auth');
 const clog = require('../utils/colorLogging');
 
-
+// get the test route for the timeline graph
+router.get('/timeline', async(req, res) => {
+    try{
+        let transactionObjs = Transaction.findAll({
+            nested: true,
+            all: true
+        })
+        let transactions = transactionObjs.map((transactionObj) => {
+            return transactionObj.get({plain: true});
+        })
+        res.render('graphs', {transactions});
+    }catch(err){
+        res.status(500).json({message:"Failed to return timeline"});
+    }
+})
 
 module.exports = router;
