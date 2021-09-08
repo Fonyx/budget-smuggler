9a445e612cb5415636c85b5c8d52a80277f11fae
Arne Tripolone <arnetripolone@gmail.com>
2021-09-08 18:46:22 +1000

    updated update balance js

--- public/js/update-balance.js
+++ public/js/update-balance.js
@@ -1,30 +1,37 @@
+console.log("infile")
 const updateBalanceFormHandler = async (event) => {
+    console.log("infunction")
     event.preventDefault();
     // collect values from the login form
     const balance = document.querySelector('#current-balance').value.trim();
-
+    
+    const userid = document.querySelector('#current-balance').dataset.id;
     if(balance){
-
-        // consume the login endpoint with a post request
-        const response = await fetch('user/profile/balance', {
-            method: 'PUT',
-            body: JSON.stringify({balance}),
-            headers: {'Content-Type':'application/json'}
-        });
-
-        if(response.ok){
-            console.log('User balance successfully retrieved');
-            //document.location.replace('/profile');
-        } else {
-            console.log('User current balance not retrieved');
-            alert(response.statusText);
+        try{
+            // consume the login endpoint with a post request
+            const response = await fetch(`/transaction/update/${userid}`, {
+                method: 'PUT',
+                body: JSON.stringify({balance}),
+                headers: {'Content-Type':'application/json'}
+            });
+            if(response.ok){
+                console.log('User balance successfully retrieved');
+                //document.location.replace('/profile');
+            } else if (response.statusCode === 400) {
+                    alert('Client error');
+            } else if (response.statusCode === 500) {
+                    alert('Server error');
+            } else {
+                console.log('User current balance not retrieved');
+                alert(response.statusText);
+            }
+        } catch(err){
+            console.log(err)
         }
-
     } else {
         console.log('User did not submit values for current balance');
     }
 }
-
 
 // attach the submit handler to the signup button
 document
