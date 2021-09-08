2598e2b7544463a7d1c27504c954aa744568b584
Nick <nick.alex.ritchie@gmail.com>
2021-09-08 20:40:12 +1000

    building timline data route

--- controllers/graph-routes.js
+++ controllers/graph-routes.js
@@ -6,17 +6,25 @@ const clog = require('../utils/colorLogging');
 // get the test route for the timeline graph
 router.get('/timeline', async(req, res) => {
     try{
-        let transactionObjs = await Transaction.findAll({
-            nested: true,
-            all: true
-        })
-        let transactions = transactionObjs.map((transactionObj) => {
-            return transactionObj.get({plain: true});
-        })
-        res.render('graphs', {transactions});
+        res.render('graphs');
     }catch(err){
         res.status(500).json({message:"Failed to return timeline"});
     }
 });
 
+// get the data packet for the timeline route 
+router.get('/data/timeline', onlyIfLoggedIn, async(req, res)=> {
+    try{
+        let transactionObjs = await Transaction.findAll({
+            nested: true,
+            all: true
+        });
+        let transactions = transactionObjs.map((transactionObj) => {
+            return transactionObj.get({plain: true});
+        });
+        res.status(200).json(transactions, colours, );
+    }catch(err){
+
+    }
+})
 module.exports = router;
