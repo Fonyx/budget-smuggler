5d37cc8125b8ca370e84cc576a580e2f3d97fb6d
Nick <nick.alex.ritchie@gmail.com>
2021-09-08 19:14:40 +1000

    fixed server

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
