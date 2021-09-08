0bc74afb95aad0ea9d9a8918c2daf5ad808c1663
Nick <nick.alex.ritchie@gmail.com>
2021-09-08 11:40:39 +1000

    added Procfile and added seed commands for local and heroku

--- models/Transaction.js
+++ models/Transaction.js
@@ -41,6 +41,7 @@ Transaction.init(
         },
         category_name: {
             type: DataTypes.STRING,
+            allowNull: false,
             references: {
                 model: 'category',
                 key: 'name',
