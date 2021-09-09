0f0f50b45b782d7ad4c91b94f6b347974e3dff76
Arne Tripolone <arnetripolone@gmail.com>
2021-09-09 16:02:01 +1000

    Partial commit

--- public/js/update-transaction.js
+++ public/js/update-transaction.js
@@ -2,23 +2,24 @@ console.log("infile")
 const updateTransactionFormHandler = async (event) => {
     console.log("infunction")
     event.preventDefault();
-    // collect values from the login form
+    // collect values from the update-transaction form
     const name = document.querySelector('#update-transaction-name').value.trim();
     const ammount = document.querySelector('#update-transaction-ammount').value.trim();
     const dueDate = document.querySelector('#update-transaction-due-date').value.trim();
     const frequency = document.querySelector('#update-transaction-frequency').value.trim();
     const type = document.querySelector('#update-transaction-type').value.trim();
-    const endRecurrence = document.querySelector('#update-transaction-end-recurrence').value.trim();
-    const categoryName = document.querySelector('#update-transaction-category-name').value.trim();
+    const category_id = document.querySelector('#update-transaction-category-id').value.trim();
+    const user_id = document.querySelector('#update-transaction-user_id').value.trim();
     
-    //const name = document.querySelector('#update-transaction-form').dataset.id;
+    //Unsure about line 15. 
+    const transactionObj = document.querySelector('#update-transaction-name').dataset.id;
 
-    if(name & ammount & dueDate & frequency & type & endRecurrence & categoryName){
+    if(name & ammount & dueDate & frequency & type & category_id & user_id){
         try{
             // consume the login endpoint with a post request
             const response = await fetch(`/transaction/update/${transactionObj.id}`, {
                 method: 'PUT',
-                body: JSON.stringify({name, ammount, dueDate, frequency, type, endRecurrence, categoryName}),
+                body: JSON.stringify({name, ammount, dueDate, frequency, type, endRecurrence, category_id, user_id}),
                 headers: {'Content-Type':'application/json'}
             });
             if(response.ok){
@@ -43,4 +44,4 @@ const updateTransactionFormHandler = async (event) => {
 // attach the submit handler to the signup button
 document
 .querySelector('.update-transaction-form')
-.addEventListener('submit', updateTransactionFormHandler);
+.addEventListener('submit-btn', updateTransactionFormHandler);
