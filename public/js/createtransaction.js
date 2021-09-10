d0aee21f2ce7bed9ca2c3f74f1d7cd0f3feb0911
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-09-10 13:40:55 +1000

    Bug fix for create transaction form

--- public/js/createtransaction.js
+++ public/js/createtransaction.js
@@ -1,21 +1,18 @@
-console.log("infile");
-const name = document.querySelector('#transaction-name').value.trim();
-const amount = document.querySelector('#transaction-amount').value.trim();
-const type = document.querySelector('input[name="transaction-type"]:checked').value;
-const category = document.querySelector('#transaction-category').value.trim();
-const dueDate = document.querySelector('#transaction-duedate').value.trim();
-const frequency = document.querySelector('input[name="transaction-frequency"]:checked').value;
-const endDate = document.querySelector('#transaction-enddate').value.trim();
-
 const createTransactionHandler = async (event) => {
     event.preventDefault();
-    console.log("infunction");
-    const transactionObj = document.querySelector('#create-transaction-form').dataset.id;
+    const name = document.querySelector('#transaction-name').value.trim();
+    const amount = document.querySelector('#transaction-amount').value.trim();
+    const type = document.querySelector('input[name="transaction-type"]:checked').value;
+    const category = document.querySelector('#transaction-category').value.trim();
+    const dueDate = document.querySelector('#transaction-duedate').value.trim();
+    const frequency = document.querySelector('input[name="transaction-frequency"]:checked').value;
+    const endDate = document.querySelector('#transaction-enddate').value.trim();
+    const transaction_id = document.querySelector('#create-transaction-form').dataset.id;
 
-    if (name & amount & type & category & dueDate & frequency & endDate) {
-        const response = await fetch(`/transaction/create/${transactionObj.id}`, {
+    if (name && amount && type && category && dueDate && frequency && endDate && transaction_id) {
+        const response = await fetch(`/transaction/create`, {
             method: 'POST',
-            body: JSON.stringify({ name, amount, type, category, dueDate, frequency, endDate }),
+            body: JSON.stringify({ name, amount, type, category, dueDate, frequency, endDate, transaction_id }),
             headers: { 'Content-Type': 'application/json' }
         });
 
