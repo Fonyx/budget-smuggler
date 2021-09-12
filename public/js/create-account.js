1c88729c8e3838cc3d44dc221bc27082b13c03e4
Nick <nick.alex.ritchie@gmail.com>
2021-09-12 17:16:29 +1000

    add account works

--- public/js/create-account.js
+++ public/js/create-account.js
@@ -1,18 +1,18 @@
 const createAccountFormHandler = async (event) => {
     event.preventDefault();
 
-    const accountName = document.querySelector('#account-name').value.trim();
-    const accountBalance = document.querySelector('#account-balance').value.trim();
+    const name = document.querySelector('#account-name').value.trim();
+    const balance = document.querySelector('#account-balance').value.trim();
 
-    if (accountName && accountBalance) {
+    if (name && balance) {
         const response = await fetch('/account/create', {
             method: 'POST',
-            body: JSON.stringify({ accountName, accountBalance }),
+            body: JSON.stringify({ name, balance }),
             headers: { 'Content-Type': 'application/json' }
         });
 
         if (response.ok) {
-            document.location.replace('/account');
+            document.location.replace('/profile');
         } else {
             console.log(response);
             alert(response.statusText);
