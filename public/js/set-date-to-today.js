93b3c7f73cd611f7d54d982b479d268552b1dad7
Nick <nick.alex.ritchie@gmail.com>
2021-09-15 15:08:10 +1000

    trying jquery to set default due date to today

--- public/js/set-date-to-today.js
+++ public/js/set-date-to-today.js
@@ -1,1 +1,8 @@
-document.getElementById('transaction-due-date').value = new Date().toDateInputValue();
+// let dueDateEl = document.getElementById('transaction-due-date');
+
+// dueDateEl.value = new Date().toDateInputValue();
+
+$(document).ready(function() {
+    var date = Date();
+   $("#transaction-due-date").attr("placeholder",date);
+});
